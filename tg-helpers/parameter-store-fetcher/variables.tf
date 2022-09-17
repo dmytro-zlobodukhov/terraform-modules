8e8b65be6145066c3d26cb4810f66a27c777abf4
Dmytro Zlobodukhov <96909881+dmytro-zlobodukhov@users.noreply.github.com>
2022-09-17 22:47:47 +0300

    fixed variable

--- tg-helpers/parameter-store-fetcher/variables.tf
+++ tg-helpers/parameter-store-fetcher/variables.tf
@@ -1,7 +1,6 @@
 variable "parameter_name" {
   description = "Name of the parameter."
   type        = string
-  default     = "/blai/dev/apps/VERY_SECRET_PARAMS"
 }
 variable "with_decryption" {
   description = "Whether to return decrypted SecureString value. Defaults to true."
